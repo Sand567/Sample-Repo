160cf9f6b56adef40de105ed517ef7552e99901f
Sandeep Nagendra <sandynagendra@gmail.com>
2019-02-17 16:00:19 -0500

    Added comments to exception handling

--- Java8LambdaBasics/src/com/sandeep/unit2/ExceptionHandlingExample.java
+++ Java8LambdaBasics/src/com/sandeep/unit2/ExceptionHandlingExample.java
@@ -13,6 +13,9 @@ public class ExceptionHandlingExample {
 
 	}
 
+	// This is a wrapper to the existing lambda which will 
+	// move the part of handling the exceptions from the place where
+	// the lambda actually gets called which will catch the exception if it occurs
 	private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> biConsumer) {
 		return (v, k) -> {
 			try {
@@ -23,6 +26,7 @@ public class ExceptionHandlingExample {
 		};
 	}
 
+	// This method will accept the lambda and invoke the method to execute the lambda
 	private static void process(int[] numbers, int key, BiConsumer<Integer, Integer> biConsumer) {
 		for(int i : numbers) {
 			biConsumer.accept(i, key);

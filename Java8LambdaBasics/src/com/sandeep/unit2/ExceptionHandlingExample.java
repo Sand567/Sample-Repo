e237090c22341d240f928584f140acb00e087c3f
Sandeep Nagendra <sandynagendra@gmail.com>
2019-02-17 15:56:17 -0500

    Added Exception Handling to Java 8 Project

--- Java8LambdaBasics/src/com/sandeep/unit2/ExceptionHandlingExample.java
+++ Java8LambdaBasics/src/com/sandeep/unit2/ExceptionHandlingExample.java
@@ -1,22 +1,32 @@
 package com.sandeep.unit2;
 
+import java.util.function.BiConsumer;
+
 public class ExceptionHandlingExample {
 
 	public static void main(String[] args) {
 		
 		int[] numbers = {2, 6, 7, 4};
-		int key = 2;
+		int key = 0;
 		
-		process(numbers, key);
+		process(numbers, key, wrapperLambda((v, k) -> System.out.println(v / k)));
 
 	}
 
-	private static void process(int[] numbers, int key) {
-		
+	private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> biConsumer) {
+		return (v, k) -> {
+			try {
+				biConsumer.accept(v, k);
+			} catch(ArithmeticException e) {
+				System.out.println("Exception caught in wrapperLambda");
+			}
+		};
+	}
+
+	private static void process(int[] numbers, int key, BiConsumer<Integer, Integer> biConsumer) {
 		for(int i : numbers) {
-			System.out.println(i + key);
+			biConsumer.accept(i, key);
 		}
-		
 	}
 
 }

6687ac9d4bc4f2dc38809b4a93cdf4e706651629
Sandeep Nagendra <sandynagendra@gmail.com>
2019-08-11 18:33:56 -0400

    Added more content

---
+++ AllQuestions/src/com/sandeep/stringinplacereverse/StringInPlaceReverseDemo.java
@@ -0,0 +1,27 @@
+package com.sandeep.stringinplacereverse;
+
+public class StringInPlaceReverseDemo {
+
+	public static void main(String[] args) {
+		
+        String number = "1234";
+        System.out.println("original String: " + number);
+        String reversed = inPlaceReverse(number);
+        System.out.println("reversed String: " + reversed);
+
+	}
+
+	private static String inPlaceReverse(String input) {
+        final StringBuilder builder = new StringBuilder(input);
+        int length = builder.length();
+        for (int i = 0; i < length / 2; i++) {
+            final char current = builder.charAt(i);
+            final int otherEnd = length - i - 1;
+            builder.setCharAt(i, builder.charAt(otherEnd)); // swap
+            builder.setCharAt(otherEnd, current);
+        }
+
+        return builder.toString();
+	}
+
+}
---
+++ AllQuestions/src/com/sandeep/stringpermutations/StringPermutationDemo.java
@@ -0,0 +1,27 @@
+package com.sandeep.stringpermutations;
+
+public class StringPermutationDemo {
+
+	public static void main(String[] args) {
+		permutation("GOD");
+	}
+
+	public static void permutation(String input) {
+		permutation("", input);
+	}
+
+	private static void permutation(String perm, String input) {
+		
+		if (input.isEmpty()) {
+			System.out.println(perm + input);
+		} else {
+			
+			for (int i = 0; i < input.length(); i++) {
+				permutation(perm + input.charAt(i), input.substring(0, i) + input.substring(i + 1, input.length()));
+			}
+			
+		}
+		
+	}
+
+}
